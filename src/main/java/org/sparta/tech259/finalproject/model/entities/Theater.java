a37199a1c3ab4b326fcf8b4028dd391a1621d3aa
Alastair Smith <alithton@proton.me>
2024-05-28 15:03:38 +0100

    Add repository and entities for theater data

---
+++ src/main/java/org/sparta/tech259/finalproject/model/entities/Theater.java
@@ -0,0 +1,52 @@
+package org.sparta.tech259.finalproject.model.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "theaters")
+public class Theater {
+
+    @Id
+    private String id;
+    private int theaterId;
+    private Location location;
+
+    public Theater(String id, int theaterId, Location location) {
+        this.id = id;
+        this.theaterId = theaterId;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getTheaterId() {
+        return theaterId;
+    }
+
+    public void setTheaterId(int theaterId) {
+        this.theaterId = theaterId;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Theater{" +
+                "id='" + id + '\'' +
+                ", theaterId=" + theaterId +
+                ", location=" + location +
+                '}';
+    }
+}
---
+++ src/main/java/org/sparta/tech259/finalproject/model/repositories/TheaterRepository.java
@@ -0,0 +1,15 @@
+package org.sparta.tech259.finalproject.model.repositories;
+
+import org.sparta.tech259.finalproject.model.entities.Theater;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface TheaterRepository extends ListCrudRepository<Theater, String> {
+    
+    List<Theater> findByLocationAddressState(String statue);
+
+    List<Theater> findByLocationAddressCity(String city);
+    List<Theater> findByLocationAddressZipcode(String zipcode);
+    List<Theater> findByLocationAddressStreet1(String street);
+}
